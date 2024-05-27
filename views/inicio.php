3db89dd89a6c01cd569588fd705b57e211bb4e33
Dahian Nunez <dahiancatalinan@gmail.com>
2024-05-26 19:14:34 -0500

    Tercera versión de repositorio

--- views/inicio.php
+++ views/inicio.php
@@ -1,4 +1,39 @@
-<div class="content-wrapper iframe-mode" data-widget="iframe" data-loading-screen="750">
+<?php
+define("BASE URL","/parcial3Portafolio/views/");
+require_once("../config/conecxion.php");
+if (isset($_SESSION["usu_id"])){
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Tabbed IFrames</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../public/plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../public/dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="../public/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+</head>
+<body class="hold-transition sidebar-mini layout-fixed" data-panel-auto-height-mode="height">
+<div class="wrapper">
+
+  <!-- Navbar -->
+  <?php include("./modulos/header.php");?>
+  <!-- /.n avbar -->
+
+  <!-- Main Sidebar Container -->
+  <?php include("./modulos/menu.php");?>
+  
+  
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper iframe-mode" data-widget="iframe" data-loading-screen="750">
   <section class="content">
     <div class="container-fluid">
 
@@ -53,3 +88,43 @@
         </div>
       </div>
     </div>
+</section>
+
+    
+  </div>
+  <!-- /.content-wrapper -->
+  <?php include("./modulos/footer.php");?>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="../plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="../plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="../plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../dist/js/adminlte.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../dist/js/demo.js"></script>
+</body>
+</html>
+
+<?php
+  }else{
+   header("location:". conectar::ruta() ."/parcial3Portafolio/views/404.php");
+  }
+?>
--- views/modulos/menu.php
+++ views/modulos/menu.php
@@ -11,7 +11,8 @@
         <div class="image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <input type="hidden" id="usu_idx" value="<?php echo $_SESSION["usu_id"] ?>">
+          <a href="#" class="d-block"><?php echo $_SESSION["nombre"];?></a>
         </div>
       </div>
 
